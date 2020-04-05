fd83dbda0764f06c126b7e477b54c4b3ed1e76d3
Felipe Alves <falves@gazeus.com>
2020-04-05 17:31:21 -0400

    Adicionando job

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,14 @@
-Parse.Cloud.define('hello', function(req, res) {
-  return 'Hi';
-});
+ Parse.Cloud.job("update", (request) => {
+   // params: passed in the job call
+   // headers: from the request that triggered the job
+   // log: the ParseServer logger passed in the request
+   // message: a function to update the status message of the job object
+   const {
+     params,
+     headers,
+     log,
+     message
+   } = request;
+   message("I just started");
+   return doSomethingVeryLong(request);
+ });
